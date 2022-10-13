1994d74b2aea91f5b524f8eb822eab686b91d246
Che Ming Chung <cheming@ualberta.ca>
2022-10-12 18:48:01 -0600

    register models to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Author, Comment, Request, Like
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Comment)
+admin.site.register(Request)
+admin.site.register(Like)
