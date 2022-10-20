898b02c64a1a28c25ae752556d8ad73f15c09242
Che-Ming <cheming@ualberta.ca>
2022-10-20 12:02:06 -0600

    smallfix dexplayName

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -7,4 +7,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'displayName','email', 'is_active', 'created', 'updated']
-        read_only_field = ['displayName', 'is_active', 'created', 'updated']
+        read_only_field = ['is_active', 'created', 'updated']
