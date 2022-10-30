7f2fceb14ff4edc09e60378b5dfb558ac38a402d
Che-Ming <cheming@ualberta.ca>
2022-10-30 13:15:54 -0600

    Change settings to use postgres

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -89,8 +89,18 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'socialdb',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Messiwon7',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
