91fb3fe8fe4ac628dca739c757d171d45495ebb1
Che-Ming <cheming@ualberta.ca>
2022-10-31 17:56:14 -0600

    Fixed comment modal

--- frontend/src/components/PostSingular.tsx
+++ frontend/src/components/PostSingular.tsx
@@ -164,13 +164,13 @@ const PostSingular = ({post_type, post_title, post_id, source, origin, post_desc
                                 </div>:
                                 null
                             }
-                            <div onClick={openCommentModal} className="post-comments">{count} Comments...</div>
+                            <div className="post-comments">{count} Comments...</div>
                             <div className="post-interact">
                                 {editSwitch ? 
                                     <button onClick={() => setEditMode(true)} className="post-edit-button">Edit Post</button> : null
                                 }
                                 <button className="post-like-button">0 Likes</button>
-                                <button className="post-comment-button">Comment</button>
+                                <button onClick={openCommentModal} className="post-comment-button">Comment</button>
                                 <button onClick={sharePost} className="post-share-button">Share</button>
                             </div>
                         </div>:
