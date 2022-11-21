f59a2eda68412a178094bd2329c77bd2cf19047b
david-parise <parise@ualberta.ca>
2022-11-21 15:46:10 -0700

    Undo model changes

--- api/serializers.py
+++ api/serializers.py
@@ -16,5 +16,4 @@ class CommentSerializer(serializers.ModelSerializer):
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ['context', 'summary', 'type', 'author', 'object']
-        read_only_fields = ['created']
+        fields = ['context', 'summary', 'type', 'author', 'object', 'post', 'comment']
