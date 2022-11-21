3caaddee75fc622c2e836fb1380a86c3504eff1a
david-parise <parise@ualberta.ca>
2022-11-21 12:14:55 -0700

    Updated settings

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'socialdb.sqlite3',
     }
 }
 
