8181e5010cae3a2d39c1db23f78a78ab2b10f675
Che-Ming <cheming@ualberta.ca>
2022-11-20 18:20:59 -0700

    Show more fields

--- api/serializers.py
+++ api/serializers.py
@@ -7,13 +7,17 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['type', 'title', 'id', 'source', 'origin', 'description', 'contentType', 'author', 'categories', 'count', 'comments', 'published', 'visibility', 'unlisted']
+        # Set depth to 1 for author
+        depth = 1
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'type', 'author', 'post', 'comment', 'contentType', 'published']
+        depth=1
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['context', 'summary', 'type', 'author', 'object', 'post', 'comment']
+        depth=1
