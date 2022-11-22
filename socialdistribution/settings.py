65a01eab046a5d095c5535068451ae74c4365f05
david-parise <parise@ualberta.ca>
2022-11-21 21:07:34 -0700

    Update settings.py

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gywyu=1&eytj1h1)aq*t$96&@wmy8ui276(49ost5^pzs-n$bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = True
 
