871753c2f05ddcae97a79e2ebb9c6e16f3323f98
Che-Ming Chung <cheming@ualberta.ca>
2022-11-24 20:49:25 -0700

    Added tests, some fixes to output

--- api/tests/test_inbox.py
+++ api/tests/test_inbox.py
@@ -1,9 +1,58 @@
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
 
 
-class LikeTests(TestCase):
-    def test_post_likes_get(self):
-        # Try to get list of likes for a post 
+class InboxTests(TestCase):
+
+    # Test getting list of inbox items
+    def test_inbox_get(self):
+        # Create object author
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of object
+        id = body['user']['id']
+
+        # Try to get list of inbox items, should return 200 OK
+        response = self.client.get('/authors/' + id + '/inbox/')
+
+        # Make sure inbox is empty
+        self.assertEqual(len(response.data['items']), 0)
+        self.assertEqual(response.status_code, 200)
+
+    # Test follow request
+    def test_inbox_follow_request(self):
+        # Create object author
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of object
+        object_id = body['user']['id']
+
+        # Create actor author
+        data = {'displayName': "testuser2", 'password': 'test_pass', 'github':'https://github.com/2'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of actor
+        actor_id = body['user']['id']
+
+        # Create follow request
+        data = {'actor': actor_id}
+        response = self.client.post('/authors/' + object_id + '/follow_requests/', data=data, content_type='application/json')
+        
+        # Should return 201 Created
+        self.assertEqual(response.status_code, 201)
+
+        # Get inbox items for object
+        response = self.client.get('/authors/' + object_id + '/inbox/')
+        # Make sure inbox is not empty
+        self.assertEqual(len(response.data['items']), 1)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_inbox_post(self):
         # Create an author
         data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
         response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
@@ -11,6 +60,14 @@ class LikeTests(TestCase):
         body = response.json()
         # Get id of author
         id = body['user']['id']
+        # Create another author
+        data = {'displayName': "testuser2", 'password': 'test_pass', 'github':'https://github.com/a'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id2 = body['user']['id']
+
         # Create a post
         data = {
                 'categories':'ded', 
@@ -24,20 +81,36 @@ class LikeTests(TestCase):
             }
         response = self.client.put('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
+        response = self.client.get('/authors/'+ id +'/posts/1/')
+        self.assertEqual(response.status_code, 200)
+        # Get inbox items for author
+        response = self.client.get('/authors/' + id + '/inbox/')
+        # Make sure inbox is not empty
+        self.assertEqual(len(response.data['items']), 1)
+        # Make sure inbox item is a post
+        self.assertEqual(response.data['items'][0]['type'], 'post')
+        # Make sure other author also has inbox item
+        response = self.client.get('/authors/' + id2 + '/inbox/')
+        self.assertEqual(len(response.data['items']), 1)
+        self.assertEqual(response.data['items'][0]['type'], 'post')
+        self.assertEqual(response.status_code, 200)
 
-        # Get the likes for that post
-        response = self.client.get('/authors/'+ id +'/posts/1/likes/')
-        self.assertEqual(response.status_code, 200) 
-
-    def test_comment_likes_get(self):
-        # Try to get list of likes for a comment 
-        # Create an author
+    def test_inbox_comment(self):
+                # Create an author
         data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
         response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
         # Get body of response
         body = response.json()
         # Get id of author
         id = body['user']['id']
+        # Create another author
+        data = {'displayName': "testuser2", 'password': 'test_pass', 'github':'https://github.com/a'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id2 = body['user']['id']
+
         # Create a post
         data = {
                 'categories':'ded', 
@@ -58,36 +131,24 @@ class LikeTests(TestCase):
         data = {
                 'comment': 'test comment',
                 'contentType': 'text/plain',
-                'author': id,
+                'author': id2,
             }
         response = self.client.post('/authors/'+ id +'/posts/1/comments/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        # Get comment id from response
-        body = response.json()
-        comment_id = body['id']
-        # Get the comment
-        response = self.client.get('/authors/'+ id +'/posts/1/comments/'+ comment_id +'/')
-        self.assertEqual(response.status_code, 200)
 
-        # Get the likes for that comment
-        response = self.client.get('/authors/'+ id +'/posts/1/comments/'+ comment_id +'/likes/')
-        self.assertEqual(response.status_code, 200)
+        # Get inbox items for author
+        response = self.client.get('/authors/' + id + '/inbox/')
+        # Make sure inbox is not empty
+        self.assertEqual(len(response.data['items']), 2)
+        # Make sure inbox has a post and a comment
+        self.assertNotEqual(response.data['items'][0]['type'], response.data['items'][1]['type'])
 
-    def test_get_liked(self):
-        # Try getting the list of liked posts and comments for an author
-        # Create an author
-        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
-        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
-        # Get body of response
-        body = response.json()
-        # Get id of author
-        id = body['user']['id']
-        # Get the liked objects for that author
-        response = self.client.get('/authors/'+ id +'/liked/')
-        self.assertEqual(response.status_code, 200) 
+        # Make sure other author also has inbox item for post only
+        response = self.client.get('/authors/' + id2 + '/inbox/')
+        self.assertEqual(len(response.data['items']), 1)
+        self.assertEqual(response.data['items'][0]['type'], 'post')
     
-    def test_posts_like(self):
-        # Try liking a post should get 201
+    def test_inbox_post_like(self):
         # Create an author
         data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
         response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
@@ -117,8 +178,13 @@ class LikeTests(TestCase):
         response = self.client.post('/authors/'+ id +'/posts/1/likes/', data={"author": id}, content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
-    def test_comments_like(self):
-        # Try liking a comment should get 201
+        # Get the check inbox
+        response = self.client.get('/authors/'+ id +'/inbox/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['items']), 2)
+        self.assertNotEqual(response.data['items'][0]['type'], response.data['items'][1]['type'])
+    
+    def test_inbox_comment_like(self):
         # Create an author
         data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
         response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
@@ -161,3 +227,7 @@ class LikeTests(TestCase):
         response = self.client.post('/authors/'+ id +'/posts/1/comments/'+ comment_id +'/likes/', data={"author": id}, content_type='application/json')
         self.assertEqual(response.status_code, 201)
         
+        # Get the check inbox
+        response = self.client.get('/authors/'+ id +'/inbox/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['items']), 3)
--- api/tests/test_posts.py
+++ api/tests/test_posts.py
@@ -4,27 +4,37 @@ class PostTests(TestCase):
     def test_posts_get_list(self):
         # Try to get list of posts, should return 200 OK
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        response = self.client.post('/authors/', data=data, content_type='application/json')
-
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
         # Try to get list of posts for that author
-        response = self.client.get('/authors/1/posts/')
+        response = self.client.get('/authors/'+ id +'/posts/')
         self.assertEqual(response.status_code, 200)
 
     def test_posts_get_non_existing(self):
         # Try to get a non-existing post, should give 404
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        self.client.post('/authors/', data=data, content_type='application/json')
-
-        response = self.client.get('authors/1/posts/1333/')
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
+        response = self.client.get('authors/'+ id +'/posts/1333/')
         self.assertEqual(response.status_code, 404)
 
     def test_posts_create(self):
         # Try to create a post, should return 201 created
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        self.client.post('/authors/', data=data, content_type='application/json')
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
         # Create a post
         data = {
                 'categories':'ded', 
@@ -36,14 +46,41 @@ class PostTests(TestCase):
                 'visibility': 'PUBLIC', 
                 'unlisted': False
             }
-        response = self.client.post('/authors/1/posts/', data=data, content_type='application/json')
+        response = self.client.post('/authors/'+ id +'/posts/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_posts_put_create(self):
+        # Try to create a post with PUT, should return 201 created
+        # Create an author
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
+        # Create a post
+        data = {
+                'categories':'ded', 
+                'title': 'test post', 
+                'source': 'https://github.com', 
+                'origin': 'https://github.com', 
+                'description': 'test post', 
+                'contentType': 'text/plain', 
+                'visibility': 'PUBLIC', 
+                'unlisted': False
+            }
+        response = self.client.put('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_posts_get_existing(self):
         # Try to get an existing post, should return 200 OK
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        self.client.post('/authors/', data=data, content_type='application/json')
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
         # Create a post
         data = {
                 'categories':'ded', 
@@ -53,20 +90,22 @@ class PostTests(TestCase):
                 'description': 'test post', 
                 'contentType': 'text/plain', 
                 'visibility': 'PUBLIC', 
-                'unlisted': False,
-                'id': '1'
+                'unlisted': False
             }
-        self.client.post('/authors/1/posts/', data=data, content_type='application/json')
-
-        # Try to get the created post
-        response = self.client.get('/authors/1/posts/1/')
+        response = self.client.put('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/authors/'+ id +'/posts/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_posts_update(self):
         # Try to update an existing post, should return 200 OK
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        self.client.post('/authors/', data=data, content_type='application/json')
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
         # Create a post
         data = {
                 'categories':'ded', 
@@ -76,11 +115,11 @@ class PostTests(TestCase):
                 'description': 'test post', 
                 'contentType': 'text/plain', 
                 'visibility': 'PUBLIC', 
-                'unlisted': False,
-                'id': '1'
+                'unlisted': False
             }
-        self.client.post('/authors/1/posts/', data=data, content_type='application/json')
-        response = self.client.get('/authors/1/posts/1/')
+        response = self.client.put('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/authors/'+ id +'/posts/1/')
         self.assertEqual(response.status_code, 200)
 
         # Update the post
@@ -95,14 +134,21 @@ class PostTests(TestCase):
                 'unlisted': False
             }
         # set content type to json
-        response = self.client.patch('/authors/1/posts/1/', data=data, content_type='application/json')
+        response = self.client.post('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
+        # Check the title is updated
+        response = self.client.get('/authors/'+ id +'/posts/1/')
+        self.assertEqual(response.json()['title'], 'update post')
         self.assertEqual(response.status_code, 200)
 
     def test_posts_delete(self):
         # Try to update an existing post, should return 200 OK
         # Create an author
-        data = {'id':'1', 'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com'}
-        self.client.post('/authors/', data=data, content_type='application/json')
+        data = {'displayName': "testuser", 'password': 'test_pass', 'github':'https://github.com/'}
+        response = self.client.post('/api/auth/register/', data=data, content_type='application/json')
+        # Get body of response
+        body = response.json()
+        # Get id of author
+        id = body['user']['id']
         # Create a post
         data = {
                 'categories':'ded', 
@@ -112,13 +158,13 @@ class PostTests(TestCase):
                 'description': 'test post', 
                 'contentType': 'text/plain', 
                 'visibility': 'PUBLIC', 
-                'unlisted': False,
-                'id': '1'
+                'unlisted': False
             }
-        self.client.post('/authors/1/posts/', data=data, content_type='application/json')
-        response = self.client.get('/authors/1/posts/1/')
+        response = self.client.put('/authors/'+ id +'/posts/1/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/authors/'+ id +'/posts/1/')
         self.assertEqual(response.status_code, 200)
 
         # Delete the post
-        response = self.client.delete('/authors/1/posts/1/')
+        response = self.client.delete('/authors/'+ id +'/posts/1/')
         self.assertEqual(response.status_code, 204)
